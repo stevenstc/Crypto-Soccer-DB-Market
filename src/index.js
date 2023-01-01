1a92d5fb837d320ca721f9fed254ff2ceba4531c
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2023-01-01 15:31:15 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://diamondsoccer.tk');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
